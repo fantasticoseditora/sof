bf95f166a2e730ac7956e5a72b8edde3f33e09df
fantasticoseditora <fantasticoseditora@gmail.com>
2026-09-22 00:57:05 -0300

    Adiciona configuração ativa sem cache

---
+++ vale-da-inocencia/config-live.js
@@ -0,0 +1,7 @@
+window.VALE_CONFIG = Object.freeze({
+  endpoint: "https://script.google.com/macros/s/AKfycbzl-7h50AjRmlLxazmFExWjVovo85eLhyxup5OZA7fHMhP69mL26HnWf2v7JwjGexov1g/exec",
+
+  // Conecte os arquivos finais antes do início da campanha.
+  pdfUrl: "",
+  epubUrl: ""
+});
